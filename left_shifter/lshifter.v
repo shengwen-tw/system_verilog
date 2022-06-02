e3438dca3020f880b4ef56689f2a387c96e48cde
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-06-02 21:40:18 +0800

    implement left shifter

---
+++ left_shifter/lshifter.v
@@ -0,0 +1,16 @@
+module lshifter(data_in, data_out, bits);
+
+parameter data_width = 8;
+parameter shift_len = 3;
+
+input [data_width-1:0] data_in;
+input [shift_len-1:0] bits;
+output [data_width-1:0] data_out;
+
+wire [data_width-1:0] shifter1, shifter2;
+
+mux2 m1(shifter1, bits[0], data_in,  {data_in[data_width-2:0], 1'b0});
+mux2 m2(shifter2, bits[1], shifter1, {shifter1[data_width-3:0], 2'b0});
+mux2 m3(data_out, bits[2], shifter2, {shifter2[data_width-5:0], 4'b0});
+
+endmodule
---
+++ left_shifter/top.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "Vlshifter.h"
+#include "verilated.h"
+
+using namespace std;
+
+bool test_left_shifter(Vlshifter* lsh, uint8_t data, uint8_t bits)
+{
+	lsh->data_in = data;
+	lsh->bits = bits;
+	lsh->eval();
+
+	uint8_t true_ans = data << bits;
+
+	if(lsh->data_out == true_ans) {
+		printf("[left shifter] %d << %d = %d\n",
+                       lsh->data_in, lsh->bits, lsh->data_out);
+		return true;
+	} else {
+		printf("[error] %d MM %d = %d (expect %d)\n",
+                       lsh->data_in, lsh->bits, lsh->data_out, true_ans);
+		return false;
+	}
+}
+
+int main(int argc, char** argv, char** env)
+{
+	VerilatedContext* contextp = new VerilatedContext;
+	contextp->commandArgs(argc, argv);
+
+	Vlshifter* lsh = new Vlshifter{contextp};
+
+	/* unit test */
+
+	printf("[left shifter] unit test begins.\n\r");
+
+	for(int i = 0; i < 256; i++) {
+		for(int j = 0; j < 8; j++) {
+			bool correct = test_left_shifter(lsh, i, j);
+			if(correct == false) {
+				delete lsh;
+				delete contextp;
+				return 0;
+			}
+		}
+	}
+
+	printf("[left shifter] uint tests all passed.\n\r");
+
+	delete lsh;
+	delete contextp;
+
+	return 0;
+}

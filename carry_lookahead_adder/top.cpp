179ba68c8a22e0b57c4165911563e87565748671
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-06-03 01:26:54 +0800

    implement carry-lookahead adder

---
+++ carry_lookahead_adder/top.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <limits.h>
+#include "Vcarry_lookahead_adder.h"
+#include "verilated.h"
+
+using namespace std;
+
+int main(int argc, char** argv, char** env)
+{
+	VerilatedContext* contextp = new VerilatedContext;
+	contextp->commandArgs(argc, argv);
+
+	Vcarry_lookahead_adder* cla = new Vcarry_lookahead_adder{contextp};
+
+	/* unit test */
+
+	printf("[carry-lookahead adder] unit test begins.\n\r");
+
+	int test_range = UCHAR_MAX;
+
+	for(int i = 0; i < test_range; i++) {
+		for(int j = 0; j < test_range; j++) {
+			cla->a = i;
+			cla->b = j;
+			cla->eval();
+
+			uint8_t true_ans = i + j;
+			if(cla->s == true_ans) {
+				printf("[passed] a=%d, b=%d => s=%d\n",
+                                       cla->a, cla->b, cla->s);
+			} else {
+				printf("[failed] a=%d, b=%d => s=%d (expect s=%d)\n",
+                                       cla->a, cla->b, cla->s, true_ans);
+
+				delete cla;
+				delete contextp;
+
+				return 0;
+			}
+		}
+	}
+
+	printf("[carry-lookahead adder] unit tests all passed.\n\r");
+
+	delete cla;
+	delete contextp;
+
+	return 0;
+}
+

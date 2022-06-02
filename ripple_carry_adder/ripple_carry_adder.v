8390640d1b3dbd7a09ca11c1d969c6c1369a938d
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-06-02 12:07:01 +0800

    rearrange code of the ripple carry adder

--- ripple_carry_adder/ripple_carry_adder.v
+++ ripple_carry_adder/ripple_carry_adder.v
@@ -2,12 +2,12 @@ module ripple_carry_adder(A, B, S, Cout);
 
 parameter bit_width = 8;
 
-input [bit_width-1:0] A;
-input [bit_width-1:0] B;
+input  [bit_width-1:0] A;
+input  [bit_width-1:0] B;
 output [bit_width-1:0] S = 0;
-output  Cout = 0;
+output Cout = 0;
 
-wire [bit_width:0] Cin;
+wire   [bit_width:0] Cin;
 assign Cin[0] = 1'b0;
 
 genvar i;
